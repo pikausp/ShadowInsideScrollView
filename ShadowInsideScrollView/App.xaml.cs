3a3001de294d6a610b5d77ac53fe561421a1a9cd
Petr Pikaus <pikaus.petr@gmail.com>
2023-12-08 19:58:52 +0100

    Add project files.

---
+++ ShadowInsideScrollView/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShadowInsideScrollView
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShadowInsideScrollView/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShadowInsideScrollView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
